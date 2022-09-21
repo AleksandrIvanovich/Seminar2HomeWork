c31e3c6790cec71704d62f34d84db3ba71dd9bd5
Aleksandr <fomichov73@mail.ru>
2022-09-21 22:55:07 +0300

    Added task003

---
+++ HomeWork2/Task003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число от одного до семи:");
+int num = int.Parse(Console.ReadLine());
+if (num < 1 || num > 7)
+{
+    Console.WriteLine("Вы ввели некоректное число!");
+}
+if (num > 5)
+{
+    Console.WriteLine("Да!");
+}
+if (num < 5)
+{
+    Console.WriteLine("Нет!");
+}
