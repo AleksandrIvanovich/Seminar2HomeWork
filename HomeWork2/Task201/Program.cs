3d298787d9922d997cc134af2efe96710ffd83fc
Aleksandr <fomichov73@mail.ru>
2022-09-21 22:49:09 +0300

    Added task 1

---
+++ HomeWork2/Task201/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трёхзначное число:");
+int num = int.Parse(Console.ReadLine());
+int a = num % 100;
+int b = num % 10;
+int result = (a - b) / 10;
+
+if (num < 100 || num >1000)
+{
+    Console.WriteLine("Введено некорректное число!");
+}
+else
+{
+    Console.WriteLine(result);
+}
+
